6b35ce036c1035e23dc2af3f1a71ab524e04f672
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2026-01-22 17:15:44 +0100

    MAJ fichier sql + fix gen hash

--- sae_sql.sql
+++ sae_sql.sql
@@ -1,6 +1,11 @@
-DROP TABLE IF EXISTS utilisateurs;
+USE sae_ecommerce;
+
+DROP TABLE IF EXISTS ligne_panier;
+DROP TABLE IF EXISTS ligne_commande;
 DROP TABLE IF EXISTS vetements;
 DROP TABLE IF EXISTS commandes;
+DROP TABLE IF EXISTS tailles;
+DROP TABLE IF EXISTS type_vetements;
 DROP TABLE IF EXISTS etats;
 
 CREATE TABLE utilisateurs
@@ -15,26 +20,6 @@ CREATE TABLE utilisateurs
     PRIMARY KEY (id_utilisateur)
 );
 
-CREATE TABLE taille (
-    id_taille INT AUTO_INCREMENT,
-    libelle_taille VARCHAR(5),
-    PRIMARY KEY (id_taille)
-);
-
-CREATE TABLE type_vetement (
-    id_type_vetement INT AUTO_INCREMENT,
-    libelle_type_vetement VARCHAR(30),
-    PRIMARY KEY (id_type_vetement)
-);
-
-CREATE TABLE vetements
-(
-    id_vetement   INT AUTO_INCREMENT,
-    nom_vetement  VARCHAR(50),
-    prix_vetement NUMERIC(17, 2),
-    PRIMARY KEY (id_vetement)
-);
-
 CREATE TABLE etats
 (
     id_etat      INT AUTO_INCREMENT,
@@ -42,45 +27,71 @@ CREATE TABLE etats
     PRIMARY KEY (id_etat)
 );
 
+CREATE TABLE type_vetements
+(
+    id_type_vetement      INT AUTO_INCREMENT,
+    libelle_type_vetement VARCHAR(30),
+    PRIMARY KEY (id_type_vetement)
+);
+
+CREATE TABLE tailles
+(
+    id_taille      INT AUTO_INCREMENT,
+    libelle_taille VARCHAR(5),
+    PRIMARY KEY (id_taille)
+);
+
 CREATE TABLE commandes
 (
     id_commande    INT AUTO_INCREMENT,
     date_achat     DATE,
-    utilisateur_id INT NOT NULL,
-    etat_id        INT NOT NULL,
+    id_utilisateur INT NOT NULL,
+    id_etat        INT NOT NULL,
     PRIMARY KEY (id_commande),
-    CONSTRAINT fk_commande_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id_utilisateur),
-    CONSTRAINT fk_etat_utilisateur FOREIGN KEY (etat_id) REFERENCES etats (id_etat)
+    CONSTRAINT fk_commande_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs (id_utilisateur),
+    CONSTRAINT fk_commande_etat FOREIGN KEY (id_etat) REFERENCES etats (id_etat)
+);
+
+CREATE TABLE vetements
+(
+    id_vetement      INT AUTO_INCREMENT,
+    nom_vetement     VARCHAR(50),
+    prix_vetement    DECIMAL(17, 2),
+    matiere          VARCHAR(50),
+    description      TEXT,
+    id_type_vetement INT NOT NULL,
+    id_taille        INT NOT NULL,
+    PRIMARY KEY (id_vetement),
+    CONSTRAINT fk_vetement_type_vetement FOREIGN KEY (id_type_vetement) REFERENCES type_vetements (id_type_vetement),
+    CONSTRAINT fk_vetement_taille FOREIGN KEY (id_taille) REFERENCES tailles (id_taille)
 );
 
 CREATE TABLE ligne_commande
 (
-    commande_id INT NOT NULL,
-    vetement_id INT NOT NULL,
-    prix        NUMERIC(17, 2),
-    quantite    NUMERIC(15, 0),
-    PRIMARY KEY (commande_id),
-    PRIMARY KEY (vetement_id),
-    CONSTRAINT fk_commande FOREIGN KEY (commande_id) REFERENCES commandes (id_commande),
-    CONSTRAINT fk_vetement_commande FOREIGN KEY (vetement_id) REFERENCES vetements (id_vetement)
+    id_commande INT,
+    id_vetement INT,
+    prix        DECIMAL(17, 2),
+    quantite    DECIMAL(15, 0),
+    PRIMARY KEY (id_commande, id_vetement),
+    CONSTRAINT fk_ligne_commande_commande FOREIGN KEY (id_commande) REFERENCES commandes (id_commande),
+    CONSTRAINT fk_ligne_commande_vetement FOREIGN KEY (id_vetement) REFERENCES vetements (id_vetement)
 );
 
 CREATE TABLE ligne_panier
 (
-    utilisateur_id INT NOT NULL,
-    vetement_id    INT NOT NULL,
-    quantite       NUMERIC(15, 0),
+    id_utilisateur INT,
+    id_vetement    INT,
     date_ajout     DATE,
-    PRIMARY KEY (utilisateur_id),
-    PRIMARY KEY (vetement_id),
-    CONSTRAINT fk_utilisateur_panier FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id_utilisateur),
-    CONSTRAINT fk_vetement_panier FOREIGN KEY (vetement_id) REFERENCES vetements (id_vetement)
+    quantite       DECIMAL(15, 0),
+    PRIMARY KEY (id_utilisateur, id_vetement),
+    CONSTRAINT fk_ligne_panier_utilisateur FOREIGN KEY (id_utilisateur) REFERENCES utilisateurs (id_utilisateur),
+    CONSTRAINT fk_ligne_panier_vetement FOREIGN KEY (id_vetement) REFERENCES vetements (id_vetement)
 );
 
 INSERT INTO utilisateurs (login, password, email, role, est_actif, nom)
-VALUES ('jeancharles',
-        'pbkdf2:sha256:1000000$FZ08GBBmtgQTsnIK$2d883189b974b2e176c6fb94da7e3d0e38bc944286f0cad23780b9ef664055b2',
-        'email@test.com', 'ROLE_client', true, 'Jean'),
-       ('Noey',
-        'pbkdf2:sha256:1000000$S70M3Zo0lYqQp89j$7637da8a363c3e7265fdf01f47e4bdd55ad7f91d8902d98bce16d2ea8a92a889',
-        'email@test.com', 'ROLE_admin', true, 'Noe');
+VALUES ('admin',
+        'pbkdf2:sha256:600000$muqytMe8Zt47VPtf$918a68ecb18be6e804826f1bb8582a7fa80fd0684daa792b462bf64720ec89a3',
+        'admin@email.com', 'ROLE_admin', true, 'admin'),
+       ('client',
+        'pbkdf2:sha256:600000$Tn78W35fe8hsMj5v$ea2c5ec88c61fb4fb6853e7956af7bbcab0068e03381551e8d3b178446e4d30b',
+        'client@test.com', 'ROLE_client', true, 'client');
