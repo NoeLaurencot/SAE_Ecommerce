ef7fa796852f1aaeaabaf27182ab3f3b7eaf26f6
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2026-01-22 16:06:03 +0100

    ajout methode hash

--- gen_hash_pw.py
+++ gen_hash_pw.py
@@ -3,3 +3,4 @@ from werkzeug.security import generate_password_hash, check_password_hash
 pw = input("entrer mpd: ")
 
 print(generate_password_hash(pw, method='pbkdf2:sha256'))
+check_password_hash()
