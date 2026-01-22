99ae1423919adfb1a9851f2dddc3d8d4f25e38c3
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2026-01-22 16:33:37 +0100

    Ajout LOGO DE LA MORT QUI TUE

--- sae_sql.sql
+++ sae_sql.sql
@@ -1,46 +1,86 @@
-DROP TABLE IF EXISTS users;
-DROP TABLE IF EXISTS articles;
-DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS utilisateurs;
+DROP TABLE IF EXISTS vetements;
+DROP TABLE IF EXISTS commandes;
+DROP TABLE IF EXISTS etats;
 
-CREATE TABLE users (
-    id_user INT AUTO_INCREMENT,
-    login VARCHAR(50),
-    password VARCHAR(255),
-    email VARCHAR(255),
-    role VARCHAR(25),
-    is_active BOOLEAN,
-    name VARCHAR(50),
-    PRIMARY KEY (id_user)
+CREATE TABLE utilisateurs
+(
+    id_utilisateur INT AUTO_INCREMENT,
+    login          VARCHAR(50),
+    password       VARCHAR(255),
+    email          VARCHAR(255),
+    role           VARCHAR(25),
+    est_actif      BOOLEAN,
+    nom            VARCHAR(50),
+    PRIMARY KEY (id_utilisateur)
 );
 
-CREATE TABLE articles (
-    id_article INT AUTO_INCREMENT,
-    PRIMARY KEY (id_article)
+CREATE TABLE taille (
+    id_taille INT AUTO_INCREMENT,
+    libelle_taille VARCHAR(5),
+    PRIMARY KEY (id_taille)
 );
 
-CREATE TABLE status (
-    id_status INT AUTO_INCREMENT,
-    status_label VARCHAR(25),
-    PRIMARY KEY (id_status)
+CREATE TABLE type_vetement (
+    id_type_vetement INT AUTO_INCREMENT,
+    libelle_type_vetement VARCHAR(30),
+    PRIMARY KEY (id_type_vetement)
 );
 
-CREATE TABLE orders (
-    id_order INT AUTO_INCREMENT,
-    date_purshase DATE,
-    user_id INT NOT NULL,
-    status_id INT NOT NULL,
-    PRIMARY KEY (id_order),
-    CONSTRAINT fk_order_user FOREIGN KEY (user_id) REFERENCES users (id_user),
-    CONSTRAINT fk_status_user FOREIGN KEY (status_id) REFERENCES status (id_status)
+CREATE TABLE vetements
+(
+    id_vetement   INT AUTO_INCREMENT,
+    nom_vetement  VARCHAR(50),
+    prix_vetement NUMERIC(17, 2),
+    PRIMARY KEY (id_vetement)
 );
 
-CREATE TABLE order_line (
-    order_id INT NOT NULL,
-    article_id INT NOT NULL,
-    price NUMERIC(17,2),
-    quantity NUMERIC(15,0)
+CREATE TABLE etats
+(
+    id_etat      INT AUTO_INCREMENT,
+    libelle_etat VARCHAR(25),
+    PRIMARY KEY (id_etat)
 );
 
-INSERT INTO users (login, password, email, role, is_active, name)
-VALUES ('jeancharles','pbkdf2:sha256:1000000$FZ08GBBmtgQTsnIK$2d883189b974b2e176c6fb94da7e3d0e38bc944286f0cad23780b9ef664055b2','email@test.com', 'ROLE_client',true, 'Jean'),
-       ('Noey','pbkdf2:sha256:1000000$S70M3Zo0lYqQp89j$7637da8a363c3e7265fdf01f47e4bdd55ad7f91d8902d98bce16d2ea8a92a889','email@test.com', 'ROLE_admin',true, 'Noe');
+CREATE TABLE commandes
+(
+    id_commande    INT AUTO_INCREMENT,
+    date_achat     DATE,
+    utilisateur_id INT NOT NULL,
+    etat_id        INT NOT NULL,
+    PRIMARY KEY (id_commande),
+    CONSTRAINT fk_commande_utilisateur FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id_utilisateur),
+    CONSTRAINT fk_etat_utilisateur FOREIGN KEY (etat_id) REFERENCES etats (id_etat)
+);
+
+CREATE TABLE ligne_commande
+(
+    commande_id INT NOT NULL,
+    vetement_id INT NOT NULL,
+    prix        NUMERIC(17, 2),
+    quantite    NUMERIC(15, 0),
+    PRIMARY KEY (commande_id),
+    PRIMARY KEY (vetement_id),
+    CONSTRAINT fk_commande FOREIGN KEY (commande_id) REFERENCES commandes (id_commande),
+    CONSTRAINT fk_vetement_commande FOREIGN KEY (vetement_id) REFERENCES vetements (id_vetement)
+);
+
+CREATE TABLE ligne_panier
+(
+    utilisateur_id INT NOT NULL,
+    vetement_id    INT NOT NULL,
+    quantite       NUMERIC(15, 0),
+    date_ajout     DATE,
+    PRIMARY KEY (utilisateur_id),
+    PRIMARY KEY (vetement_id),
+    CONSTRAINT fk_utilisateur_panier FOREIGN KEY (utilisateur_id) REFERENCES utilisateurs (id_utilisateur),
+    CONSTRAINT fk_vetement_panier FOREIGN KEY (vetement_id) REFERENCES vetements (id_vetement)
+);
+
+INSERT INTO utilisateurs (login, password, email, role, est_actif, nom)
+VALUES ('jeancharles',
+        'pbkdf2:sha256:1000000$FZ08GBBmtgQTsnIK$2d883189b974b2e176c6fb94da7e3d0e38bc944286f0cad23780b9ef664055b2',
+        'email@test.com', 'ROLE_client', true, 'Jean'),
+       ('Noey',
+        'pbkdf2:sha256:1000000$S70M3Zo0lYqQp89j$7637da8a363c3e7265fdf01f47e4bdd55ad7f91d8902d98bce16d2ea8a92a889',
+        'email@test.com', 'ROLE_admin', true, 'Noe');
