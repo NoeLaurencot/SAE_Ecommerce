aa4ede20b6776640873a759b6baca0424b7855d5
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2026-01-24 18:09:11 +0100

    ajout alertes

---
+++ static/js/alerts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+	let alerts = document.getElementsByClassName("alert");
+
+	for (let i = 0; i < alerts.length; i++) {
+		let alert = alerts[i];
+		alert.style.left = "0";
+		alert.style.bottom = (20 + i * 75) + "px";
+
+		let closeBtn = alert.querySelector("button");
+		closeBtn.addEventListener("click", function() {
+			alert.style.left = "-100%";
+			setTimeout(function() {
+				alert.remove();
+			}, 500);
+		});
+	}
+
+
+});
