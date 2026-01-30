6af2c78829d60f7ca4f243109943edfb49e43ca6
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2026-01-30 17:51:58 +0100

    Fix style

--- static/js/hamburger-menu.js
+++ static/js/hamburger-menu.js
@@ -1,35 +1,45 @@
+
 let menuBtn = document.getElementById("nav-burger-menu");
 let menuContainer = document.getElementById("burger-menu");
 let screenBlur = document.getElementById("screen-blur");
 
-menuBtn.addEventListener("click", function () {
-    menuBtn.classList.toggle("burger-menu-button-active");
-    menuContainer.classList.toggle("burger-menu-active");
-    screenBlur.classList.toggle("screen-blur-active");
+function openMenu() {
+    menuBtn.classList.add("burger-menu-button-active");
+    menuContainer.classList.add("burger-menu-active");
+    screenBlur.classList.add("screen-blur-active");
     document.body.style.overflow = 'hidden';
     menuContainer.focus();
+}
+
+function closeMenu() {
+    menuBtn.classList.remove("burger-menu-button-active");
+    menuContainer.classList.remove("burger-menu-active");
+    screenBlur.classList.remove("screen-blur-active");
+    document.body.style.overflow = '';
+    menuBtn.focus();
+}
+
+menuBtn.addEventListener("click", function () {
+    if (menuContainer.classList.contains("burger-menu-active")) {
+        closeMenu();
+    } else {
+        openMenu();
+    }
 });
 
 screenBlur.addEventListener("click", function () {
-    menuBtn.classList.toggle("burger-menu-button-active");
-    menuContainer.classList.toggle("burger-menu-active");
-    screenBlur.classList.toggle("screen-blur-active");
-    document.body.style.overflow = '';
+    closeMenu();
 });
 
 document.addEventListener("keydown", function (event) {
     if (event.key === 'Escape' && menuContainer.classList.contains("burger-menu-active")) {
-        menuBtn.classList.toggle("burger-menu-button-active");
-        menuContainer.classList.toggle("burger-menu-active");
-        screenBlur.classList.toggle("screen-blur-active");
-        document.body.style.overflow = '';
+        closeMenu();
     }
-
     if (event.key === 'Enter' && document.activeElement === menuBtn) {
-        menuBtn.classList.toggle("burger-menu-button-active");
-        menuContainer.classList.toggle("burger-menu-active");
-        screenBlur.classList.toggle("screen-blur-active");
-        document.body.style.overflow = 'hidden';
-        menuContainer.focus();
+        if (menuContainer.classList.contains("burger-menu-active")) {
+            closeMenu();
+        } else {
+            openMenu();
+        }
     }
 });
