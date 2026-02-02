067d5f0dbbe894b5d89ecb07fed7ade84030fd62
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2026-02-02 13:22:18 +0100

    Changement html + ajout db port

--- connexion_db.py
+++ connexion_db.py
@@ -8,6 +8,7 @@ db_login = os.environ["DB_LOGIN"]
 db_name = os.environ["DB_NAME"]
 db_password = os.environ["DB_PASSWORD"]
 db_host = os.environ["DB_HOST"]
+db_port = int(os.environ["DB_PORT"])
 
 def get_db():
     if 'db' not in g:
@@ -16,6 +17,7 @@ def get_db():
             user=db_login,
             password=db_password,
             database=db_name,
+            port=db_port,
             charset='utf8mb4',
             cursorclass=pymysql.cursors.DictCursor
         )
