9d41dd8dc9c1bdb22252e832b208e690b815df69
NoeLaurencot <noe.laurencot@edu.univ-fcomte.fr>
2026-04-10 12:18:04 +0200

    fix bug input quantité

--- static/js/vetement-input.js
+++ static/js/vetement-input.js
@@ -11,17 +11,34 @@ for (let i = 0; i < arrowUpArr.length; i++) {
 
     arrowUp.addEventListener('click', function () {
         let input = arrowUp.parentElement.querySelector('input');
-        let value = parseInt(input.value);
-        let max = parseInt(input.max);
-        if (value < max) {
+        let value = parseInt(input.value, 10);
+        let min = parseInt(input.min, 10);
+        let max = parseInt(input.max, 10);
+
+        if (Number.isNaN(min)) {
+            min = 0;
+        }
+        if (Number.isNaN(value)) {
+            value = min;
+        }
+
+        if (Number.isNaN(max) || value < max) {
             input.value = value + 1;
         }
     });
 
     arrowDown.addEventListener('click', function () {
         let input = arrowDown.parentElement.querySelector('input');
-        let value = parseInt(input.value);
-        let min = parseInt(input.min);
+        let value = parseInt(input.value, 10);
+        let min = parseInt(input.min, 10);
+
+        if (Number.isNaN(min)) {
+            min = 0;
+        }
+        if (Number.isNaN(value)) {
+            value = min;
+        }
+
         if (value > min) {
             input.value = value - 1;
         }
